96f6f1b9fa54af65d6a64cb828953fa0d44601ee
citadelict <chidiaustin78@gmail.com>
2024-07-29 00:42:58 +0100

    new config

--- PBL/variables.tf
+++ PBL/variables.tf
@@ -28,5 +28,57 @@ variable "enable_classiclink_dns_support" {
 }
 
 variable "preferred_number_of_public_subnets" {
-    default = null
+    # default = null
+     type        = number
+  description = "Number of public subnets"
+}
+
+variable "preferred_number_of_private_subnets" {
+    # default = null
+    type        = number
+  description = "Number of private subnets"
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "name" {
+  description = "Base name to use for naming the subnets"
+  type        = string
+}
+
+variable "ami" {
+  type        = string
+  description = "AMI ID for the launch template"
+}
+
+
+variable "keypair" {
+  type        = string
+  description = "key pair for the instances"
+}
+
+variable "account_no" {
+  type        = number
+  description = "the account number"
+}
+
+
+variable "master-username" {
+  type        = string
+  description = "RDS admin username"
+}
+
+variable "master-password" {
+  type        = string
+  description = "RDS master password"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+
 }
